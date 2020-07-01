3a28f1eb8461014cf795e10f8fd04cdb791a30b7
Yoram Shamir <yoram.shamir@cybereason.com>
2020-07-01 21:39:29 +0300

    add makefile

--- setup.py
+++ setup.py
@@ -1,50 +1,57 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from __future__ import print_function
-from setuptools import setup
-import io
+"""
+Package PyTestCenter for distribution.
+"""
+
+from setuptools import setup, find_packages
 
 
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
+def main():
+
+    with open('requirements.txt') as f:
+        install_requires = f.read().splitlines()
+    with open('README.rst') as f:
+        long_description = f.read()
+
+    setup(
+        name='pytestcenter',
+        description='Python OO API package to automate Spirent TestCenter (STC) traffic generator',
+        url='https://github.com/shmir/PyTestCenter/',
+        use_scm_version={
+            'root': '.',
+            'relative_to': __file__,
+            'local_scheme': 'node-and-timestamp'
+        },
+
+        license='Apache Software License',
+
+        author='Yoram Shamir',
+        author_email='yoram@ignissoft.com',
+
+        platforms='any',
+        install_requires=install_requires,
+        packages=find_packages(exclude=('tests', 'tests.*',)),
+        include_package_data=True,
+
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+
+        keywords='testcenter STC l2l3 test tool spirent automation',
+
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Natural Language :: English',
+            'License :: OSI Approved :: Apache Software License',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Topic :: Software Development :: Testing',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'],
+    )
 
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-install_requires = [r for r in required if r and r[0] != '#' and not r.startswith('git')]
-
-setup(
-    name='pytestcenter',
-    version='3.0.0',
-    url='https://github.com/shmir/PyTestCenter/',
-    license='Apache Software License',
-    author='Yoram Shamir',
-    install_requires=install_requires,
-    author_email='yoram@ignissoft.com',
-    description='Python OO API package to automate Spirent TestCenter (STC) traffic generator',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    charset='UTF-8',
-    variant='GFM',
-    packages=['testcenter', 'testcenter.samples', 'testcenter.api'],
-    include_package_data=True,
-    platforms='any',
-    tests_require=['pytest'],
-    classifiers=[
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Testing :: Traffic Generation',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        ])
+if __name__ == '__main__':
+    main()
